29a145fd871c5ba54e9a6597634382d9221281e3
pprg1996 <pprg1996@gmail.com>
2018-10-13 00:09:36 -0400

    Buttons setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import appStyle from "./app.module.css";
+import buttonsStyle from "./buttons.module.css";
+import buttonStyle from "./button.module.css";
+
+function Button({ symbol }) {
+  return (
+    <button type="button" className={`${buttonStyle.number}`}>{symbol}</button>
+  );
+}
+
+function Buttons() {
+  return (
+    <div className={buttonsStyle.buttons}>
+      <Button symbol="Sqrt" />
+      <Button symbol="^" />
+      <Button symbol="Del" />
+      <Button symbol="/" />
+      <Button symbol="7" />
+      <Button symbol="8" />
+      <Button symbol="9" />
+      <Button symbol="X" />
+      <Button symbol="4" />
+      <Button symbol="5" />
+      <Button symbol="6" />
+      <Button symbol="-" />
+      <Button symbol="1" />
+      <Button symbol="2" />
+      <Button symbol="3" />
+      <Button symbol="+" />
+      <Button symbol="+/-" />
+      <Button symbol="0" />
+      <Button symbol="." />
+      <Button symbol="=" />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className={appStyle.app}>
+      <Buttons />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
